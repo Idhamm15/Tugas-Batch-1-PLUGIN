1b378358b1d7b4bfc34942eb4437f2fcc0e620fb
Idhamm15 <idhamtamvanz123@gmail.com>
2021-11-25 09:40:41 +0700

    tugas pertemnuan 4

---
+++ pertemuan 4/index.js
@@ -0,0 +1,41 @@
+// var nilai;
+var nilairaya = 80;
+var nilaiGenta = 50;
+
+if (nilairaya >= 80) {
+  alert("indeksnya adalah A");
+} else if (nilairaya && nilairaya < 80) {
+  alert("indeksnya adalah B");
+} else if (nilairaya  >= 60 && nilairaya  < 70) {
+  alert("indeksnya adalah C");
+} else if (nilairaya  > 50 && nilairaya  <= 60) {
+  alert("indeksnya adalah D");
+} else {
+  alert("indeksnya adalah E");
+}
+
+// var nilaiGenta = 50;
+
+if (nilaiGenta >= 80) {
+  alert("indeksnya adalah A");
+} else if ( nilaiGenta >= 70 && nilaiGenta < 80) {
+  alert("indeksnya adalah B");
+} else if (nilaiGenta >= 60 && nilaiGenta < 70) {
+  alert("indeksnya adalah C");
+} else if (nilaiGenta >= 50 && nilaiGenta < 60) {
+  alert("indeksnya adalah D");
+} else {
+  alert("indeksnya adalah E");
+}
+
+
+// var x = 70;
+// if(x >= 80){
+// alert("Nilai = A");
+// }else if(x >= 70 && x < 80){
+// alert("Nilai = B");
+// }else if(x >= 60 && x < 70){
+// alert("Nilai = C");
+// }else{
+// alert("Nilai = D");
+// }
